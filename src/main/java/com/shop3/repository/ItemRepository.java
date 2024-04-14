079d76d25d0b2dd9b174e7d07354632dbcf1ccad
shun <riptaggart@naver.com>
2024-04-14 15:55:36 +0900

    querydsl 테스트

--- src/main/java/com/shop3/repository/ItemRepository.java
+++ src/main/java/com/shop3/repository/ItemRepository.java
@@ -3,10 +3,13 @@ package com.shop3.repository;
 import com.shop3.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 
-public interface ItemRepository extends JpaRepository<Item, Long>{
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
 
     List<Item> findByItemNm(String itemNm);
 
@@ -16,6 +19,13 @@ public interface ItemRepository extends JpaRepository<Item, Long>{
 
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
 
+    @Query("select i from Item i where i.itemDetail like " +
+            "%:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+    @Query(value="select * from item i where i.item_detail like " +
+            "%:itemDetail% order by i.price desc", nativeQuery = true)
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
 
 
 }
