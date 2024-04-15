57edb3197bccb616c5af75e5ecd217f7638e83be
shun <riptaggart@naver.com>
2024-04-16 08:39:20 +0900

    file, itemimg, itemSave

--- src/main/java/com/shop3/Shop3Application.java
+++ src/main/java/com/shop3/Shop3Application.java
@@ -2,11 +2,11 @@ package com.shop3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class Shop3Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/shop3/controller/ThymeleafTestController.java
+++ src/main/java/com/shop3/controller/ThymeleafTestController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.shop3.dto.ItemDto;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
