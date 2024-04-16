e4cfc2a27107435c0a270f743d9f243409e49294
shun <riptaggart@naver.com>
2024-04-16 16:31:57 +0900

    order cancel

--- src/test/java/com/shop3/service/OrderServiceTest.java
+++ src/test/java/com/shop3/service/OrderServiceTest.java
@@ -1,6 +1,7 @@
 package com.shop3.service;
 
 import com.shop3.constant.ItemSellStatus;
+import com.shop3.constant.OrderStatus;
 import com.shop3.dto.OrderDto;
 import com.shop3.entity.Item;
 import com.shop3.entity.Member;
@@ -74,6 +75,25 @@ class OrderServiceTest {
         int totalPrice = orderDto.getCount() * item.getPrice();
 
         assertEquals(totalPrice, order.getTotalPrice());
+    }
+
+    @Test
+    @DisplayName("order cancel test")
+    public void cancelOrder(){
+        Item item = saveItem();
+        Member member = saveMember();
+
+        OrderDto orderDto = new OrderDto();
+        orderDto.setCount(10);
+        orderDto.setItemId(item.getId());
+        Long orderId = orderService.order(orderDto, member.getEmail());
+
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(EntityNotFoundException::new);
+        orderService.cancelOrder(orderId);
+
+        assertEquals(OrderStatus.CANCEL, order.getOrderStatus());
+        assertEquals(100, item.getStockNumber());
     }
 
 }
